1db1ad17dde856fbce298ee7e90a4cfc984d72d3
[Rajeevyaji] <[rajeevyaji@gmail.com]>
2025-09-02 20:29:04 +0200

    added info

--- sample.py
+++ sample.py
@@ -2,7 +2,22 @@ import pandas as pd
 import numpy as np
 
 print( 'sample')
-hjfgkjhfgy
 
-sdggfjhg
-khsdgfl
+"In this project, you'll imagine you are a data scientist interested in exploring data that looks at how certain diagnostic factors affect the diabetes outcome of women patients.\n",
+    "\n",
+    "You will use your EDA skills to help inspect, clean, and validate the data.\n",
+    "\n",
+    "**Note**: This [dataset](https://www.kaggle.com/uciml/pima-indians-diabetes-database) is from the National Institute of Diabetes and Digestive and Kidney Diseases. It contains the following columns:\n",
+    "\n",
+    "- `Pregnancies`: Number of times pregnant\n",
+    "- `Glucose`: Plasma glucose concentration per 2 hours in an oral glucose tolerance test\n",
+    "- `BloodPressure`: Diastolic blood pressure\n",
+    "- `SkinThickness`: Triceps skinfold thickness\n",
+    "- `Insulin`: 2-Hour serum insulin\n",
+    "- `BMI`: Body mass index\n",
+    "- `DiabetesPedigreeFunction`: Diabetes pedigree function\n",
+    "- `Age`: Age (years)\n",
+    "- `Outcome`: Class variable (0 or 1)\n",
+    "\n",
+    "Let's get started!"
+   ]
